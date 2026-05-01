b816eef6f052a00f0ad810211599e23c07990348
cseabhisheks <singhabhishek.engineer@gmail.com>
2026-05-01 22:05:56 +0530

    navbar is created

---
+++ my-app/src/component/StatCard.jsx
@@ -0,0 +1,38 @@
+import { FaFileAlt } from "react-icons/fa";
+
+const StatCard = ({
+  title,
+  value,
+  subtitle,
+  icon: Icon = FaFileAlt, // default icon
+}) => {
+  return (
+    <div className="bg-white rounded-xl border border-gray-200 p-5 w-full max-w-sm shadow-sm hover:shadow-md transition">
+      
+      {/* Top Section */}
+      <div className="flex justify-between items-start">
+        <h3 className="text-sm font-medium text-gray-600">
+          {title}
+        </h3>
+
+        <div className="text-gray-400 text-lg">
+          <Icon />
+        </div>
+      </div>
+
+      {/* Value */}
+      <div className="mt-4">
+        <h2 className="text-3xl font-semibold text-gray-900">
+          {value}
+        </h2>
+      </div>
+
+      {/* Subtitle */}
+      <p className="text-sm text-gray-500 mt-1">
+        {subtitle}
+      </p>
+    </div>
+  );
+};
+
+export default StatCard;
---
+++ my-app/src/layout/Navbar.jsx
@@ -0,0 +1,52 @@
+import { MdOutlineArrowDropDownCircle } from "react-icons/md";
+
+
+const Navbar = () => {
+  return (
+    <div className="w-full bg-white shadow-sm px-6 py-4 flex items-center justify-between rounded-xl">
+      
+      {/* LEFT SIDE */}
+      <div className="flex items-center gap-4">
+        
+        {/* Logo */}
+        <div className="w-12 h-12 bg-gradient-to-r from-indigo-500 to-purple-600 rounded-xl flex items-center justify-center">
+          <span className="text-white text-xl font-bold">📘</span>
+        </div>
+
+        {/* Title */}
+        <div>
+          <h1 className="text-xl font-semibold text-gray-800">
+            EduDoc Portal
+          </h1>
+          <p className="text-sm text-gray-500">
+            Document Management System
+          </p>
+        </div>
+      </div>
+
+      {/* RIGHT SIDE */}
+      <div className="flex items-center gap-4">
+        
+        {/* Avatar */}
+        <div className="w-10 h-10 rounded-full bg-indigo-100 flex items-center justify-center text-indigo-600 font-semibold">
+          JD
+        </div>
+
+        {/* User Info */}
+        <div className=" flex-col hidden md:flex">
+          <span className="text-sm font-medium text-gray-800">
+            John Doe
+          </span>
+          <span className="text-xs bg-green-100 text-green-600 px-2 py-0.5 rounded-full w-fit">
+            Student
+          </span>
+        </div>
+
+        {/* Dropdown Icon */}
+        < MdOutlineArrowDropDownCircle className="text-gray-500 w-5 h-5 cursor-pointer" />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
